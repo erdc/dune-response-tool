446941717f158f79d4b8cb00ab0b808819bb224b
ncohn <nicholas.t.cohn@gmail.com>
2024-09-10 17:21:30 -0400

    Morphology resolving version of code

---
+++ run_drt.m
@@ -0,0 +1,19 @@
+%load structure
+load example_input_and_output.mat
+
+%modify shoreline change rate
+scenario.grids.morphometrics.shorechange = -1; %update to -1 m/yr SCR
+
+%run model
+scenario = drt_model_instantaneous(scenario);
+
+%plot model results
+figure, 
+hold on
+cmap = jet(500);
+for idx = 1:500
+    plot(scenario.grids.XGrid, scenario.output.Z(:, idx), 'Color', cmap(idx,:))
+end
+xlabel('Cross Shore Distance (m)')
+ylabel('Elevation (m)')
+
